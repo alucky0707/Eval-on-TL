5ec3eddbd021bee81602fe44ec72b77faea57324
Alucky <alucky0707@myopera.com>
2013-06-12 00:40:10 +0900

    Fix sending tweet safety

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var
+util = require('util'),
 ntwitter = require('ntwitter'),
 html = require('ent'),
-lleval = require('./lleval.js');
+lleval = require('lleval'),
+csharp = require('./csharp.js'),
+dlang = require('./dlang.js');
 
 var
 config = require('./config.json'),
-is_tweet_valid = new RegExp('^\\s*@' + config.screen_name + '\\s*');
+is_tweet_valid = new RegExp('^\\s*(\\.?)\\s*@' + config.screen_name + '\\s+');
 
 var
 twitter = new ntwitter(config.keys);
@@ -33,12 +36,14 @@ function startStream() {
 function onTweet(data) {
   var
   lang, src,
+  rt_flag,
   tweet_id = data.id_str,
   screen_name = data.user.screen_name,
   text = html.decode(data.text);
   if(is_tweet_valid.test(text)) {
     console.log('on tweet');
-    text = text.replace(is_tweet_valid, '').match(/^(\w+)\s*((?:.|\n)*)$/);
+    rt_flag = !!text.match(is_tweet_valid)[1];
+    text = text.replace(is_tweet_valid, '').match(/^(\w+)\s*([\w\W]*)$/);
     if(text === null) return;
     lang = text[1];
     src = text[2];
@@ -47,17 +52,40 @@ function onTweet(data) {
       lleval.languages(function(err, langs) {
         if(err) {
           console.log(err);
+          onError('languages', err);
           return;
         }
         replyTo(screen_name, tweet_id, Object.keys(langs).join(','));
+      });
+    }else if(lang === 'csharp' || lang === 'cs'){
+      csharp.evaluate(src, function(err, res) {
+        if(err) {
+          console.log(err);
+          onError('evaluate', err);
+          return;
+        }
+        replyTo(screen_name, tweet_id, res.output + '\n' + res.stderr + '\n' + res.cmpinfo);
+        if(rt_flag) retweet(tweet_id);
+      });
+    }else if(lang === 'dlang' || lang === 'd'){
+      dlang.evaluate(src, function(err, res) {
+        if(err) {
+          console.log(err);
+          onError('evaluate', err);
+          return;
+        }
+        replyTo(screen_name, tweet_id, res.output + '\n' + res.stderr + '\n' + res.cmpinfo);
+        if(rt_flag) retweet(tweet_id);
       });
     }else{
       lleval.evaluate(lang, src, function(err, res) {
         if(err) {
           console.log(err);
+          onError('evaluate', err);
           return;
         }
         replyTo(screen_name, tweet_id, res.stdout + '\n' + res.stderr);
+        if(rt_flag) retweet(tweet_id);
       });
     }
   }
@@ -71,6 +99,7 @@ function onEvent(data) {
       twitter.createFriendship(data.source.screen_name,function(err, data) {
         if(err) {
           console.log(err);
+          onError('follow', err);
           return;
         }
         console.log('done re-follow');
@@ -80,13 +109,36 @@ function onEvent(data) {
   }
 }
 
-function replyTo(screen_name, tweet_id, text) {
-  twitter.updateStatus('@' + screen_name + ' ' + text, {in_reply_to_status_id: tweet_id}, function(err, res) {
+function onError(name, err) {
+  console.log('on error');
+  twitter.updateStatus(util.format('@%s %s:%j', config.developer_screen_name, name, err).slice(0,140), function(err, res) {
     if(err) {
       console.log(err);
       return;
     }
+    console.log('done error-report');
+  });
+}
+
+function replyTo(screen_name, tweet_id, text) {
+  twitter.updateStatus(('@' + screen_name + ' ' + text.replace(/@/g, "@ ")).slice(0, 140), {in_reply_to_status_id: tweet_id}, function(err, res) {
+    if(err) {
+      console.log(err);
+      onError('reply', err);
+      return;
+    }
     console.log('done reply');
+  });
+}
+
+function retweet(tweet_id) {
+  twitter.retweetStatus(tweet_id, function(err, res) {
+    if(err) {
+      console.log(err);
+      onError('retweet', err);
+      return;
+    }
+    console.log('done retweet');
   });
 }
 
